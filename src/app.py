5767cd10e2a36e8a320160c36e91a23e558edd0a
Leonardo Muzí <leomuzicarvalho@gmail.com>
2023-07-20 11:23:18 +0000

    implement user JWT auth example

--- src/app.py
+++ src/app.py
@@ -1,18 +1,23 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import hashlib
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
-from utils import APIException, generate_sitemap
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from utils import APIException, generate_sitemap, get_hash
 from admin import setup_admin
+from sqlalchemy.orm import sessionmaker
 from models import db, User
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+app.config["JWT_SECRET_KEY"] = "super-capybara"
+jwt = JWTManager(app)
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
@@ -26,24 +31,54 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/create-user', methods=['POST'])
+def handle_create_user():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
 
+    if not email or not password:
+        return jsonify({ "msg": "No password or email present." }), 201
+    
+    new_user = User(password=get_hash(password), email=email)
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "User created"
     }
 
-    return jsonify(response_body), 200
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(response_body), 201
+
+@app.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    # Query your database for email and password
+    user = User.query.filter_by(email=email, password=get_hash(password)).first()
+    if user is None:
+        # the user was not found on the database
+        return jsonify({"msg": "Bad email or password"}), 401
+    
+    # create a new token with the user id inside
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
+
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    return jsonify({"id": user.id, "email": user.email }), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
